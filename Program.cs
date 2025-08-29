62e5e51a55e5153dbb353f618b54d348f3108e2f
KaenguruuDev <user.jakob@outlook.de>
2025-08-30 00:11:34 +0200

    Change port

--- Program.cs
+++ Program.cs
@@ -5,7 +5,11 @@ sealed class Program
 	static async Task Main(string[] args)
 	{
 		var host = Host.CreateDefaultBuilder(args)
-			.ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>()).Build();
+			.ConfigureWebHostDefaults(webBuilder =>
+			{
+				webBuilder.UseStartup<Startup>();
+				webBuilder.UseUrls("http://localhost:2052");
+			}).Build();
 		await host.RunAsync();
 	}
 }
