c4d95401205c414421aa842d1c8ed3ecf582e057
KaenguruuDev <user.jakob@outlook.de>
2025-08-29 23:46:11 +0200

    Last few changes

--- Program.cs
+++ Program.cs
@@ -1,9 +1,3 @@
-using SixLabors.ImageSharp;
-using SixLabors.Fonts;
-using SixLabors.ImageSharp.Drawing.Processing;
-using SixLabors.ImageSharp.PixelFormats;
-using SixLabors.ImageSharp.Processing;
-
 namespace RTOSharpRankCards;
 
 sealed class Program
@@ -13,71 +7,5 @@ sealed class Program
 		var host = Host.CreateDefaultBuilder(args)
 			.ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>()).Build();
 		await host.RunAsync();
-
-		return;
-		const string avatarUrl =
-			"https://cdn.discordapp.com/avatars/772827007247319071/e749f88875364f7f73c31542896ab5b8.png?size=256";
-		using var client = new HttpClient();
-
-		byte[]? avatarBytes = null;
-		try
-		{
-			avatarBytes = await client.GetByteArrayAsync(avatarUrl);
-		}
-		catch
-		{
-		}
-
-		var avatar = avatarBytes != null ? Image.Load(avatarBytes) : new Image<Rgba32>(128, 128, Color.DimGray);
-
-		avatar.Mutate(x => x.Resize(new ResizeOptions
-		{
-			Size = new Size(256, 256),
-			Mode = ResizeMode.Crop
-		}));
-
-		using var img = new Image<Rgba32>(1326, 400, Color.Transparent);
-
-		var collection = new FontCollection();
-		var family = collection.Add("SmoochSans-Medium.ttf");
-		var smallFont = family.CreateFont(48);
-		var bigFont = family.CreateFont(62);
-
-		img.Mutate(ctx =>
-		{
-			var avatarPos = new Point(72, 72);
-			const int avatarSize = 256;
-			const float cornerRadius = 20f;
-
-			var rect = new RectangleF(0, 0, 1326, 400);
-			ctx.FillRoundedRectangle(Color.Black, rect, cornerRadius);
-
-			using var roundedAvatar =
-				avatar.Clone(a => a.ConvertToAvatar(new Size(avatarSize, avatarSize), avatarSize / 2f));
-
-			ctx.DrawImage(roundedAvatar, avatarPos, 1f);
-
-			ctx.DrawText("Herr Doctor", bigFont, Color.White, new PointF(365, 105));
-			ctx.DrawText("Level 49", smallFont, Color.White, new PointF(365, 190));
-
-			var rankTextSize = TextMeasurer.MeasureSize("Rank 1", new TextOptions(bigFont));
-			var xpToNextLevelSize = TextMeasurer.MeasureSize("114.66k / 117.39k XP", new TextOptions(smallFont));
-
-			ctx.DrawText("Rank 1", bigFont, Color.White, new PointF(1254 - rankTextSize.Width, 105));
-			ctx.DrawText("114.66k / 117.39k XP", smallFont, Color.White,
-				new PointF(1254 - xpToNextLevelSize.Width, 190));
-
-			const int barX = 365;
-			const int barY = 252;
-			const float barWidth = 889f;
-			const float barHeight = 62f;
-			var progress = (float)Math.Clamp(114.66 / 117.39, 0.0, 1.0);
-			var barColor = Color.ParseHex("#D7A037");
-
-			ctx.FillRoundedRectangle(barColor.Darken(0.8f), new RectangleF(barX, barY, barWidth, barHeight), 5);
-			ctx.FillRoundedRectangle(barColor, new RectangleF(barX, barY, barWidth * progress, barHeight), 5);
-		});
-
-		await img.SaveAsPngAsync("rankcard.png");
 	}
 }
--- Startup.cs
+++ Startup.cs
@@ -1,8 +1,17 @@
+using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Http.HttpResults;
+using SixLabors.Fonts;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Drawing.Processing;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+
 namespace RTOSharpRankCards;
 
 public class Startup
 {
 	private readonly string _indexTemplate = File.ReadAllText("index.html");
+	private readonly HttpClient _client = new();
 
 	public void ConfigureServices(IServiceCollection services)
 		=> services.AddControllers();
@@ -16,7 +25,7 @@ public class Startup
 		app.UseEndpoints(endpoints =>
 		{
 			endpoints.MapGet("/", GetIndex);
-			endpoints.MapGet("/cards/{username}", GetCard);
+			endpoints.MapGet("/cards/{path}", GetCard);
 			endpoints.MapPost("/card/new", CreateCard);
 		});
 	}
@@ -24,12 +33,146 @@ public class Startup
 	private async Task GetIndex(HttpContext context)
 		=> await context.Response.WriteAsync(_indexTemplate);
 
-	private async Task GetCard(HttpContext context, string username)
+	private async Task GetCard(HttpContext context, string path)
 	{
-		
+		var userPath = GetSafeFilePath("cards", Regex.Replace(path, @"_(\d+)_(\d+)\.png$", ""));
+		if (!File.Exists(userPath))
+		{
+			context.Response.StatusCode = 404;
+			return;
+		}
+
+		context.Response.Headers.Append("Content-Disposition", $"attachment; filename={userPath}");
+		context.Response.Headers.Append("Content-Type", $"image/png");
+		await using var stream = File.OpenRead(userPath);
+		await stream.CopyToAsync(context.Response.Body);
 	}
-	
+
 	private async Task CreateCard(HttpContext context)
 	{
+		var data = await context.Request.ReadFromJsonAsync<CreateCardRequestData>();
+		if (data is null)
+		{
+			context.Response.StatusCode = 400;
+			await context.Response.WriteAsync("INVALID DATA: COULD NOT PARSE JSON");
+			return;
+		}
+
+		var path = await CreateCardFromData(data);
+		if (File.Exists(Regex.Replace(path, @"_(\d+)_(\d+)\.png$", "") + ".png"))
+		{
+			await context.Response.WriteAsync(path);
+		}
+		else
+		{
+			context.Response.StatusCode = 400;
+			await context.Response.WriteAsync($"INVALID DATA: INVALID RETURN PATH: {path}");
+		}
 	}
+
+	private async Task<string> CreateCardFromData(CreateCardRequestData data)
+	{
+		using var img = new Image<Rgba32>(1326, 400, Color.Transparent);
+
+		var collection = new FontCollection();
+		var family = collection.Add("SmoochSans-Medium.ttf");
+		var smallFont = family.CreateFont(48);
+		var bigFont = family.CreateFont(62);
+
+		byte[]? avatarBytes;
+		try
+		{
+			avatarBytes = await _client.GetByteArrayAsync(data.AvatarUrl.Replace("128", "256"));
+		}
+		catch (Exception ex)
+		{
+			return ex.Message;
+		}
+
+		var avatar = Image.Load(avatarBytes);
+		avatar.Mutate(x => x.Resize(new ResizeOptions
+		{
+			Size = new Size(256, 256),
+			Mode = ResizeMode.Crop
+		}));
+
+		img.Mutate(ctx =>
+		{
+			var avatarPos = new Point(72, 72);
+			const int avatarSize = 256;
+			const float cornerRadius = 20f;
+
+			var rect = new RectangleF(0, 0, 1326, 400);
+			ctx.FillRoundedRectangle(Color.Black, rect, cornerRadius);
+
+			using var roundedAvatar =
+				avatar.Clone(a => a.ConvertToAvatar(new Size(avatarSize, avatarSize), avatarSize / 2f));
+
+			ctx.DrawImage(roundedAvatar, avatarPos, 1f);
+
+			ctx.DrawText(data.Username, bigFont, Color.White, new PointF(365, 105));
+			ctx.DrawText($"Level {data.Level}", smallFont, Color.White, new PointF(365, 190));
+
+			var rankText = $"Rank {data.Rank}";
+			var rankTextSize = TextMeasurer.MeasureSize(rankText, new TextOptions(bigFont));
+
+			var xpToNextLevelText = $"{BeautifyXp(data.Xp)} / {BeautifyXp(data.XpToNextLevel)} XP";
+			var xpToNextLevelTextSize = TextMeasurer.MeasureSize(xpToNextLevelText, new TextOptions(smallFont));
+
+			ctx.DrawText(rankText, bigFont, Color.White, new PointF(1254 - rankTextSize.Width, 105));
+			ctx.DrawText(xpToNextLevelText, smallFont, Color.White,
+				new PointF(1254 - xpToNextLevelTextSize.Width, 190));
+
+			const int barX = 365;
+			const int barY = 252;
+			const float barWidth = 889f;
+			const float barHeight = 62f;
+			var progress = data.Progress / 100f;
+			var barColor = Color.ParseHex(data.CardColor);
+
+			ctx.FillRoundedRectangle(barColor.Darken(0.8f), new RectangleF(barX, barY, barWidth, barHeight), 5);
+			ctx.FillRoundedRectangle(barColor, new RectangleF(barX, barY, barWidth * progress, barHeight), 5);
+		});
+
+		await img.SaveAsPngAsync(GetSafeFilePath("cards", $"{data.Username}"));
+		return GetSafeFilePath("cards", $"{data.Username}_{data.Level}_{data.Xp}");
+	}
+
+	private static string SanitizeUsername(string username)
+	{
+		if (string.IsNullOrWhiteSpace(username))
+			throw new ArgumentException("Username cannot be empty");
+
+		var invalidChars = Path.GetInvalidFileNameChars();
+		foreach (var c in invalidChars)
+			username = username.Replace(c, '_');
+
+		username = Regex.Replace(username, @"(\.\.|[\\/])", "_");
+
+		return username;
+	}
+
+	private static string GetSafeFilePath(string baseDirectory, string username, string extension = ".png")
+	{
+		var safeName = SanitizeUsername(username);
+		return Path.Combine(baseDirectory, safeName + extension);
+	}
+
+	private static string BeautifyXp(int xp)
+	{
+		if (xp < 1000)
+			return xp.ToString();
+
+		return xp < 1000000 ? $"{MathF.Round(xp / 1000f, 2)}k" : $"{MathF.Round(xp / 1000000f, 2)}m";
+	}
+
+	private record CreateCardRequestData(
+		string Username,
+		string AvatarUrl,
+		int Xp,
+		int Level,
+		int Rank,
+		int XpToNextLevel,
+		int Progress,
+		string CardColor);
 }
